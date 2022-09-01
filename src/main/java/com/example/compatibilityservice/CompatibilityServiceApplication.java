6eb3590a52fe3d76a1b9951e6cdd0c54bf372e01
Joshua Singer <joshuasinger@eworldes.com>
2022-08-31 20:08:19 -1000

    Initial Commit

---
+++ src/main/java/com/example/compatibilityservice/CompatibilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.compatibilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompatibilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompatibilityServiceApplication.class, args);
+    }
+
+}
